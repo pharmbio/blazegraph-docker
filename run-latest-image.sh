962462998b9d25a572636f40b8c104531fc0a4e0
Samuel Lampa <samuel.lampa@gmail.com>
2017-08-28 16:05:22 +0200

    Show the service URL from the blazegraph log when running the docker container

--- run-latest-image.sh
+++ run-latest-image.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -P -i -t $(docker images | head -n 2 | tail -n 1 | awk '{ print $3 }') /bin/bash
+docker run -P -i -t $(docker images | head -n 2 | tail -n 1 | awk '{ print $3 }') /bin/bash -ic 'tail -n 1 /var/log/blazegraph/blazegraph.log'
